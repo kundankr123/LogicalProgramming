36d71b5ea9868bab0d08161af90edb336fa11753
kundan <goswamikundan123@gmail.com>
2023-02-13 20:13:52 +0530

    Adding file which displays Greeting Message.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProgramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programming.");
+        }
+    }
+}
